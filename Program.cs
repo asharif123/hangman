9f3e686ca1f3b989b728532d58c8e29ec51e9f0c
asharif123 <awadsharif9@gmail.com>
2023-11-18 22:21:04 -0800

    fixed indentation in code

--- Program.cs
+++ Program.cs
@@ -8,8 +8,6 @@ Keep track of how many wrong guesses the player has done and use this number to 
 Added Difficulty: Increase the length of the words and choose more complex unknown words to have the player guess.
 
 //STEPS:
-
-
 //write a block to determine if a character is part of a string
 //write a block that outputs current state  of game (ex: _ _ N _ E _). Console.Clear() can help make things look better
 //Look at Console.Readkey() to make things look nicer
@@ -31,7 +29,7 @@ namespace hangman
             const int NO_MORE_GUESSES_LEFT = 0;
             //Fill a list with possible words
             var hangmanWords = new List<string>(){"jazz", "buzz", "lightyear", "cloud", "quiz", "scatter", "die", "excuse",
-            "zinc", "number", "lucky", "amber", "cherish", "brisk", "bounty", "chili", "chilly", "chili", "ghost", "gross", 
+            "zinc", "number", "lucky", "amber", "cherish", "brisk", "bounty", "chili", "chilly", "chili", "ghost", "gross",
              "harvest", "mask", "musk", "must", "parade", "plenty", "savory", "season", "spicy", "trail", "zesty", "zebra",
             "ocean", "sea", "kingkong", "pingpong", "abruptly", "absurd", "abyss", "affix"};
 
@@ -40,14 +38,13 @@ namespace hangman
             //2 second delay
             System.Threading.Thread.Sleep(2000);
             Random rng = new Random();
-            int randomWord = rng.Next(0, hangmanWords.Count+1);
+            int randomWord = rng.Next(0, hangmanWords.Count + 1);
             //print random word
             string chosenWord = hangmanWords[randomWord];
             int guessesLeft = NUMBER_OF_TRIES;
             Console.Write($"You have {NUMBER_OF_TRIES} tries to guess all the letters of the chosen word!\n");
 
             //use while loop to track user's guesses
-            //once user enters letter, check if letter is in the randomly chosen word
             //if user guesses a letter correctly, show letter in blank line
             //else, decrement Number of Tries
             //if user prints all letters before number of tries, say success!
@@ -55,15 +52,16 @@ namespace hangman
             while (guessesLeft > NO_MORE_GUESSES_LEFT)
             {
                 Console.WriteLine("\nPlease enter a letter: \n");
+                //once user enters letter, check if letter is in the randomly chosen word
                 string userGuess = Console.ReadLine().Trim().ToLower();
                 if (chosenWord.Contains(userGuess))
                 {
-                    Console.WriteLine("Great!\n");
+                    Console.ReadKey();
                 }
                 else
                 {
-                    Console.WriteLine($"\nSorry, {userGuess} does not exist!");
-                    Console.WriteLine($"\nYou have {guessesLeft-1} guesses left!");
+                    Console.WriteLine($"\nSorry, the letter {userGuess} is not in the word!");
+                    Console.WriteLine($"\nYou have {guessesLeft - 1} guesses left!\n");
                     //decrement guessesLeft if user enters incorrect letter
                     guessesLeft -= 1;
                 }
